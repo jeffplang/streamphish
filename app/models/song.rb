506fb44aeeab8f09d824e400bcdb5f888b6cdec0
Jeff Lang <jeffplang@gmail.com>
2012-09-22 15:58:07 -0600

    better organized Song model, added methods to attempt population of position and song_collections

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,27 +1,37 @@
 class Song < ActiveRecord::Base
 
+  #############
+  # Attributes
+  #############
   FILE_NAME_HASH_SECRET = "CROUOPQNDKUCBVYTQYQLUSKCOMJAQFEWXMEX"
+  attr_accessible :show_id, :title, :position, :song_file, :song_collection_ids
 
-  attr_accessible :show_id, :title, :position, :song_file, :song_collection_id, :song_collection_ids
-
+  ########################
+  # Associations & Scopes
+  ########################
+  has_and_belongs_to_many :song_collections
   has_many :section_markers
   belongs_to :show
-  has_and_belongs_to_many :song_collections
-
+ 
+  default_scope order :position
   has_attached_file :song_file,
     :url => "/system/:class/:attachment/:id_partition/:style/:hash.:extension",
     :hash_secret => FILE_NAME_HASH_SECRET
 
+  ##############
+  # Validations
+  ##############
   validates_attachment :song_file, :presence => true,
     :content_type => {:content_type => ['application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3']}
-
   validates_presence_of :show, :title, :position
+  validate :require_at_least_one_song_collection
 
+  ############
+  # Callbacks
+  ############
+  before_validation :populate_song_collection, :populate_position
   before_save :check_file_dirty
-
   after_save :set_duration
-
-  default_scope order(:position)
 
   protected
 
@@ -41,5 +51,29 @@ class Song < ActiveRecord::Base
         save
       end
     end
+  end
+
+  def populate_song_collection
+    p 'do we need to populate...'
+    if self.song_collections.empty?
+      p 'we do!'
+      sc = SongCollection.where("lower(title) = ?", self.title.downcase)
+      if sc
+        p 'populating!'
+        self.song_collections << sc
+      else
+        p 'not populating...'
+      end
+    end
+  end
+
+  def populate_position
+    unless self.position
+      self.position = Song.where(:show_id => show.id).last.position + 1
+    end
+  end
+
+  def require_at_least_one_song_collection
+    errors.add(:song_collections, "Please add at least one song collection") if song_collections.empty?
   end
 end
