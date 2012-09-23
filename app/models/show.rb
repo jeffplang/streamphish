3cb9e55076bf11a51e3d2379534abad695313d88
Jeff Lang <jeffplang@gmail.com>
2012-09-23 03:00:10 -0600

    refactored Show#to_s method

--- app/models/show.rb
+++ app/models/show.rb
@@ -17,11 +17,7 @@ class Show < ActiveRecord::Base
   friendly_id :show_date
 
   def to_s
-    "#{show_date} - #{location}"
-  end
-  alias_method :title, :to_s # for rails admin
-
-  def to_page_title_s
     "#{show_date.strftime('%m-%d-%Y')} - #{location}"
   end
+  alias_method :title, :to_s # for rails admin
 end
