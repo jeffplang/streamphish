64a2112e4546c40c11f3e246a365e3542de20879
Jeff Lang <jeffplang@gmail.com>
2012-09-22 20:52:15 -0600

    code cleanup in Song

--- app/models/song.rb
+++ app/models/song.rb
@@ -25,25 +25,16 @@ class Song < ActiveRecord::Base
   validates_attachment :song_file, :presence => true,
     :content_type => {:content_type => ['application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3']}
   validates_presence_of :show, :title, :position
+  validates_uniqueness_of :position, :scope => :show_id
   validate :require_at_least_one_song_collection
 
   ############
   # Callbacks
   ############
   before_validation :populate_song_collection, :populate_position
-  before_save :check_file_dirty
   after_save :set_duration
 
   protected
-
-  def check_file_dirty
-    if song_file.dirty?
-      Rails.logger.info "* * * IT IS DIRTY * * *"
-    else
-      Rails.logger.info "* * * NOT DIRTY * * *"
-    end
-    true
-  end
 
   def set_duration
     unless self.duration # this won't record the correct duration if we're uploading a new file
@@ -55,22 +46,17 @@ class Song < ActiveRecord::Base
   end
 
   def populate_song_collection
-    p 'do we need to populate...'
     if self.song_collections.empty?
-      p 'we do!'
       sc = SongCollection.where("lower(title) = ?", self.title.downcase)
-      if sc
-        p 'populating!'
-        self.song_collections << sc
-      else
-        p 'not populating...'
-      end
+      self.song_collections << sc if sc
     end
   end
 
   def populate_position
-    unless self.position
-      self.position = Song.where(:show_id => show.id).last.position + 1
+    if !self.position && !(last_song = Song.where(:show_id => show.id).last).nil?
+      self.position = last_song.position + 1
+    elsif !self.position
+      self.position = 1
     end
   end
 
