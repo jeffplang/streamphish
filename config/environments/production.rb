f4bb6056d2128a00337105cbb6d799a07920fc3f
Jeff Lang <jeffplang@gmail.com>
2014-03-02 15:23:58 -0500

    use uglifier for compressing js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Streamphish::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.js_compressor = Uglifier.new({ :output => { :comments => :none } })
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
